a44627a48382616ed5c8fe2fcfff05a6135c6d5f
Thomas Petrini <thomaslpetrini@gmail.com>
2015-06-22 23:03:38 +0000

    added devise

--- Gemfile
+++ Gemfile
@@ -1,9 +1,8 @@
 source 'https://rubygems.org'
 
-
+gem 'devise', '>= 3.2.4'
 
 gem 'bootstrap-sass', '~> 3.3.4'
-
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,9 @@
 Flixter::Application.configure do
+
+  config.action_mailer.default_url_options = { :host => 'flixter-tom-petrini.herokuapp.com' }
+
+  flixter-tom-petrini.herokuapp.com
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
