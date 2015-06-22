65ed96ba74c450c0f2fbe378f0f048a9bf71c2b9
Thomas Petrini <thomaslpetrini@gmail.com>
2015-06-22 23:08:57 +0000

    fixed production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,6 @@ Flixter::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'flixter-tom-petrini.herokuapp.com' }
 
-  flixter-tom-petrini.herokuapp.com
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
